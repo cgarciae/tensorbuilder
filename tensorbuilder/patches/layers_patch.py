76b4e0ec8ee110ccc289b52b120e96c4df56f87b
Cristian Garcia <cgarcia.e88@gmail.com>
2016-11-17 20:15:07 -0500

    linear_conv2d_layer

--- tensorbuilder/patches/layers_patch.py
+++ tensorbuilder/patches/layers_patch.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import inspect
 import functools
 from tensorflow.contrib import layers
-from tensorflow.contrib.layers import fully_connected
+from tensorflow.contrib.layers import fully_connected, convolution2d
 from tensorbuilder import TensorBuilder
 from phi import Builder, utils
 
@@ -34,10 +34,20 @@ def register_layer_functions(name, f):
     @TensorBuilder.register_1("tf.contrib.layers", name + "_layer", wrapped=fully_connected, explanation=explanation) #, _return_type=TensorBuilder)
     def layer_function(*args, **kwargs):
         kwargs['activation_fn'] = f
-        return tf.contrib.layers.fully_connected(*args, **kwargs)
+        return fully_connected(*args, **kwargs)
+
+def register_conv_layer_functions(name, f):
+    explanation = """and the keyword argument `activation_fn` is set to `tf.nn.{0}`.""".format(name)
+
+    @TensorBuilder.register_1("tf.contrib.layers", name + "_conv2d_layer", wrapped=convolution2d, explanation=explanation) #, _return_type=TensorBuilder)
+    def layer_function(*args, **kwargs):
+        kwargs['activation_fn'] = f
+        return convolution2d(*args, **kwargs)
 
 for name, f in funs:
     register_layer_functions(name, f)
+    register_conv_layer_functions(name, f)
+
 
 
 #linear_layer
@@ -45,6 +55,11 @@ explanation = """and the keyword argument `activation_fn` is set to `None`."""
 
 @TensorBuilder.register_1("tf.contrib.layers", alias="linear_layer", wrapped=fully_connected, explanation=explanation) #, _return_type=TensorBuilder)
 def linear_layer(*args, **kwargs):
+    kwargs['activation_fn'] = None
+    return tf.contrib.layers.fully_connected(*args, **kwargs)
+
+@TensorBuilder.register_1("tf.contrib.layers", alias="linear_conv2d_layer", wrapped=convolution2d, explanation=explanation) #, _return_type=TensorBuilder)
+def linear_conv2d_layer(*args, **kwargs):
     kwargs['activation_fn'] = None
     return tf.contrib.layers.fully_connected(*args, **kwargs)
 
